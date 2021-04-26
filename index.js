83d9f1ae3aaa6ec758bf1f6f3e90f9fee42b14ff
danielsrod <coeng.daniel@gmail.com>
2021-04-26 09:56:48 -0400

    https ta errado, tem que ser http | tirando abreviação do app

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const port = 3333;
+
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
     return res.send('hello world');
 })
 
+app.get('/cadastro', (req, res) => {
+    return res.render('index.html');
+})
+
 app.listen(port, () => {
-    console.info(`App rodando em: https://localhost:${port}`)
+    console.info(`App rodando em: http://localhost:${port}`)
 })
