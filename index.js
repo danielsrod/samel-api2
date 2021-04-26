dd7a027ed660d4940b1a77993480e478cb74ddc2
danielsrod <coeng.daniel@gmail.com>
2021-04-26 18:27:47 -0400

    setado o middleware de renderização do html
    definido o modo em como o codigo em base64 vai ser passado do client-side para o server-side

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const express = require('express')
-const app = express();
+const express = require("express");
+const path = require("path");
+
+
 const port = 3333;
+const app = express();
+app.use(express.urlencoded({extended: true}));
 
-app.use(express.static('./public'));
+// Middleware de rendereização do HTML
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-app.get('/', (req, res) => {
-    return res.send('hello world');
-})
 
-app.get('/cadastro', (req, res) => {
-    return res.render('index.html');
-})
+app.get("/", (req, res) => {
+  console.log("GET /");
+  return res.render("home.html");
+});
+
+app.get("/cadastro", (req, res) => {
+  console.log("GET /cadastro");
+  return res.render("cadastro.html");
+});
 
 app.listen(port, () => {
-    console.info(`App rodando em: http://localhost:${port}`)
-})
+  console.info(`App rodando em: http://localhost:${port}`);
+});
--- public/script.js
+++ public/script.js
@@ -9,25 +9,8 @@ function converterParaBase64() {
             ImageElemet.src = stringBase64;
             ImageElemet.id = 'base64holder';
             document.getElementById('idImageHolder').innerHTML = ImageElemet.outerHTML;
+            document.getElementById('input_inv').value = `${stringBase64}`
         }
         readerFile.readAsDataURL(file);
     }
-}
-
-
-function enviar() {
-
-    var nome = document.getElementById('input_nome').value;
-    var telefone = document.getElementById('input_telefone').value;
-    var cpf = document.getElementById('input_cpf').value;
-    var email = document.getElementById('input_email').value;
-    var base64 = document.getElementById('base64holder').src;
-
-    console.log('usuario cadastrado');
-    console.log(nome);
-    console.log(telefone);
-    console.log(cpf);
-    console.log(email);
-    console.log(base64);
-
 }
